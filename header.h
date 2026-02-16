2fc64269c92be20c1a652a63a7742353078e8164
skibidist <146653349+sexcursed@users.noreply.github.com>
2026-02-16 15:00:05 +0500

    Add files via upload

---
+++ header.h
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <vector>
+#include <fstream>
+#include <string>
+#include <algorithm>
+
+int element(std::string s);
+int term(std::string s);
+int calc(std::string s);
+void file8();
---
+++ main.cpp
@@ -0,0 +1,6 @@
+#include "header.h"
+
+int main(){
+    file8();
+    return 0;
+}
